6e44a794aaec5da6dcf7857007702b2b243703a5
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-12 10:12:53 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
     print("sound_level:"+ input.sound_level())
-    if input.sound_level() < 6:
+    if input.sound_level() > 6:
         light.set_all(light.rgb(255, 255, 255))
     else:
         light.clear()
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
     console.log("sound_level:" + input.soundLevel())
-    if (input.soundLevel() < 6) {
+    if (input.soundLevel() > 6) {
         light.setAll(light.rgb(255, 255, 255))
     } else {
         light.clear()
