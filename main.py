ce3247d27d48bac2d1cc213f3e48d9179110aafb
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-12 10:56:39 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 while True:
     print("sound_level:"+ input.sound_level())
     if input.sound_level() > 150:
-        light.set_all(light.rgb(255, 255, 255))
+        light.show_animation(light.rainbowAnimation, 500)
     else:
         light.clear()
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 while (true) {
     console.log("sound_level:" + input.soundLevel())
     if (input.soundLevel() > 150) {
-        light.setAll(light.rgb(255, 255, 255))
+        light.showAnimation(light.rainbowAnimation, 500)
     } else {
         light.clear()
     }
