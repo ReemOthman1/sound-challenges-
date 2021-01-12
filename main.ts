b6a5748bd59f39bbec7f16a223c75b7f941d4b29
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-12 10:21:23 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
     console.log("sound_level:" + input.soundLevel())
-    if (input.soundLevel() > 120) {
+    if (input.soundLevel() > 150) {
         light.setAll(light.rgb(255, 255, 255))
     } else {
         light.clear()
