622f6df0b1329b490afa82c66fd65f986b30acf4
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-14 13:15:32 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 while True:
-    print("light level:"+ input.light_level())
-    if input.light_level() < 15:
+    if input.light_level() > 15:
         music.pew_pew.play()
     else:
         music.stop_all_sounds()
--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 while (true) {
-    console.log("light level:" + input.lightLevel())
-    if (input.lightLevel() < 15) {
+    if (input.lightLevel() > 15) {
         music.pewPew.play()
     } else {
         music.stopAllSounds()
