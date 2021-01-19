b977732a60a2925306eeb2dbfa6853efeefe7274
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-19 09:48:47 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+music.set_volume(20)
+music.play_melody("g cc c d ee e d c d e c e e f g g f e f g e c c d e e d c d ee c g g cc c d e e e d c d e c", 100)
 
-music.play_melody("g cc c d ee ", 100)
-music.play_melody("e d c de c ", 100)
-music.play_melody("e e f g ", 100)
-music.set_volume(60)
+
--- main.ts
+++ main.ts
@@ -1,4 +1,2 @@
-music.playMelody("g cc c d ee ", 100)
-music.playMelody("e d c de c ", 100)
-music.playMelody("e e f g ", 100)
-music.setVolume(60)
+music.setVolume(20)
+music.playMelody("g cc c d ee e d c d e c e e f g g f e f g e c c d e e d c d ee c g g cc c d e e e d c d e c", 100)
