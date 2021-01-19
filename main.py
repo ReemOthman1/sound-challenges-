80c2f87805694dd991ad89224037918b39419b5c
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-18 21:13:20 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-music.play_melody("g d g d c  d ", 125)
-music.play_melody("em d em d c ", 125)
-music.play_melody("em/b a g ", 125)
+music.play_melody("g cc c d ee ", 125)
+music.play_melody("e d c de c ", 125)
+music.play_melody("e e f g ", 125)
  
--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
-music.playMelody("g d g d c  d ", 125)
-music.playMelody("em d em d c ", 125)
-music.playMelody("em/b a g ", 125)
+music.playMelody("g cc c d ee ", 125)
+music.playMelody("e d c de c ", 125)
+music.playMelody("e e f g ", 125)
