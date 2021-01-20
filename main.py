e9ff65048fa316e5c557ef7d4afe511332cc5ffc
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-20 17:02:09 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 while True:
     if input.sound_level() > 130: 
-        light.set_pixel_color(0,light.rgb(255,255,255,)
-        
-    
+        light.set_pixel_color(0,light.rgb(255,255,255,))
+        light.set_pixel_color(1,light.rgb(255,255,255,))
+        light.set_pixel_color(2,light.rgb(255,255,255,))
+        light.set_pixel_color(3,light.rgb(255,255,255,))      
+        light.set_pixel_color(4,light.rgb(255,255,255,))
+        light.set_pixel_color(5,light.rgb(255,255,255,))
+        light.set_pixel_color(6,light.rgb(255,255,255,))
+        light.set_pixel_color(7,light.rgb(255,255,255,))
+        light.set_pixel_color(8,light.rgb(255,255,255,))
     if input.temperature(TemperatureUnit.FAHRENHEIT) < 68:
         music.power_down.play() 
     if input.temperature(TemperatureUnit.FAHRENHEIT) > 72:
--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
 while (true) {
-    console.log
+    if (input.soundLevel() > 130) {
+        light.setPixelColor(0, light.rgb(255, 255, 255))
+        light.setPixelColor(1, light.rgb(255, 255, 255))
+        light.setPixelColor(2, light.rgb(255, 255, 255))
+        light.setPixelColor(3, light.rgb(255, 255, 255))
+        light.setPixelColor(4, light.rgb(255, 255, 255))
+        light.setPixelColor(5, light.rgb(255, 255, 255))
+        light.setPixelColor(6, light.rgb(255, 255, 255))
+        light.setPixelColor(7, light.rgb(255, 255, 255))
+        light.setPixelColor(8, light.rgb(255, 255, 255))
+    }
+    
+    if (input.temperature(TemperatureUnit.Fahrenheit) < 68) {
+        music.powerDown.play()
+    }
+    
+    if (input.temperature(TemperatureUnit.Fahrenheit) > 72) {
+        music.powerDown.play()
+    }
+    
 }
