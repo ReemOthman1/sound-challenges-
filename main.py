f47f734b91f8219bd0a8fd7072e4f96fc0ea25b1
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-20 16:54:30 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-music.set_volume(20)
-music.play_melody("  D A D D A D G G# A D A D G G# A D A D D A D ", 130)
-
-
+while True:
+    if input.sound_level() > 130: 
+        light.set_pixel_color(0,light.rgb(255,255,255,)
+        
+    
+    if input.temperature(TemperatureUnit.FAHRENHEIT) < 68:
+        music.power_down.play() 
+    if input.temperature(TemperatureUnit.FAHRENHEIT) > 72:
+        music.power_down.play()
--- main.ts
+++ main.ts
@@ -1,2 +1,3 @@
-music.setVolume(20)
-music.playMelody("  D A D D A D G G# A D A D G G# A D A D D A D ", 130)
+while (true) {
+    console.log
+}
