b6fda260ef2e6f738bf69d65a71de4a07a6d42a9
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-21 15:34:19 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ while True:
     if input.sound_level() > 130:  
         light.show_animation(light.rainbowAnimation, 500)
         music.power_down.play_until_done()
-    if input.temperature(TemperatureUnit.FAHRENHEIT) < 68:
+    elif input.temperature(TemperatureUnit.FAHRENHEIT) < 68:
         music.power_down.play_until_done()
-    elif input.temperature(TemperatureUnit.FAHRENHEIT) > 72:   
+    if input.temperature(TemperatureUnit.FAHRENHEIT) > 72:   
         music.power_down.play_until_done()
     else:
-        music.stop_all_sounds() 
+        music.stop_all_sounds() and light.stop_all_animations() 
         
         
--- main.ts
+++ main.ts
@@ -2,14 +2,14 @@ while (true) {
     if (input.soundLevel() > 130) {
         light.showAnimation(light.rainbowAnimation, 500)
         music.powerDown.playUntilDone()
+    } else if (input.temperature(TemperatureUnit.Fahrenheit) < 68) {
+        music.powerDown.playUntilDone()
     }
     
-    if (input.temperature(TemperatureUnit.Fahrenheit) < 68) {
-        music.powerDown.playUntilDone()
-    } else if (input.temperature(TemperatureUnit.Fahrenheit) > 72) {
+    if (input.temperature(TemperatureUnit.Fahrenheit) > 72) {
         music.powerDown.playUntilDone()
     } else {
-        music.stopAllSounds()
+        music.stopAllSounds() && light.stopAllAnimations()
     }
     
 }
