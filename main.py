0d1b23a04a6b834812c5567a7ee097e66b145445
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-21 16:01:14 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 while True:
     if input.sound_level() > 130:  
         light.show_animation(light.rainbowAnimation, 500)
-        music.magic_wand.stop()
+        music.play_melody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
     elif input.temperature(TemperatureUnit.FAHRENHEIT) < 68:
         music.power_down.play_until_done()
     if input.temperature(TemperatureUnit.FAHRENHEIT) > 72:   
