1ff144675892cf67184b64fd991777efca325780
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-21 15:07:37 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -2,8 +2,6 @@ while (true) {
     if (input.soundLevel() > 130) {
         light.showAnimation(light.rainbowAnimation, 500)
         music.powerDown.playUntilDone()
-    } else {
-        light.stopAllAnimations()
     }
     
     if (input.temperature(TemperatureUnit.Fahrenheit) < 68) {
