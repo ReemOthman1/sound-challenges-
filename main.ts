535330f2883689b8f5ad9e9be8a9fc0af6bd67ad
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-21 12:56:41 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,14 +1,6 @@
 while (true) {
     if (input.soundLevel() > 130) {
-        light.setPixelColor(0, light.rgb(255, 255, 255))
-        light.setPixelColor(1, light.rgb(255, 255, 255))
-        light.setPixelColor(2, light.rgb(255, 255, 255))
-        light.setPixelColor(3, light.rgb(255, 255, 255))
-        light.setPixelColor(4, light.rgb(255, 255, 255))
-        light.setPixelColor(5, light.rgb(255, 255, 255))
-        light.setPixelColor(6, light.rgb(255, 255, 255))
-        light.setPixelColor(7, light.rgb(255, 255, 255))
-        light.setPixelColor(8, light.rgb(255, 255, 255))
+        light.showAnimation(light.rainbowAnimation, 500)
         music.powerDown.playUntilDone()
     }
     
