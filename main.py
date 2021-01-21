a55e5d00e085839e3ad8473405ea84dde3f92ce6
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-20 20:07:46 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 while True:
-    if input.sound_level() > 130 and if input.temperature(TemperatureUnit.FAHRENHEIT) < 68:
+    if input.sound_level() > 130 and input.temperature(TemperatureUnit.FAHRENHEIT) < 68:
         light.set_pixel_color(0,light.rgb(255,255,255,))
         light.set_pixel_color(1,light.rgb(255,255,255,))
         light.set_pixel_color(2,light.rgb(255,255,255,))
@@ -10,4 +10,4 @@ while True:
         light.set_pixel_color(7,light.rgb(255,255,255,))
         light.set_pixel_color(8,light.rgb(255,255,255,))
         music.power_down.play() 
-   
+       
