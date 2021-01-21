32df4313882305df09c0349662b4ce2744caa664
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-21 15:52:35 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 while (true) {
     if (input.soundLevel() > 130) {
         light.showAnimation(light.rainbowAnimation, 500)
-        music.playMelody("C C G G A A G F F E E D D C G G F F E E D G G F F E E D C C G G A A G F F E E D D C ", 150)
+        music.magicWand.stop()
     } else if (input.temperature(TemperatureUnit.Fahrenheit) < 68) {
         music.powerDown.playUntilDone()
     }
