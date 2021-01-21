29ec623b1b4b091e79704fa09f7f714d4bcf4c12
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-21 15:46:57 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 while True:
     if input.sound_level() > 130:  
         light.show_animation(light.rainbowAnimation, 500)
-        music.power_down.play_until_done()
     elif input.temperature(TemperatureUnit.FAHRENHEIT) < 68:
         music.power_down.play_until_done()
     if input.temperature(TemperatureUnit.FAHRENHEIT) > 72:   
--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 while (true) {
     if (input.soundLevel() > 130) {
         light.showAnimation(light.rainbowAnimation, 500)
-        music.powerDown.playUntilDone()
     } else if (input.temperature(TemperatureUnit.Fahrenheit) < 68) {
         music.powerDown.playUntilDone()
     }
