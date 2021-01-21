d0f2c0b37d4952bafb95427c1631cca3d5fed38c
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2021-01-20 20:10:28 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 while (true) {
-    if (input.soundLevel() > 130 && input.temperature(TemperatureUnit.Fahrenheit) < 68) {
+    if (input.soundLevel() > 130) {
         light.setPixelColor(0, light.rgb(255, 255, 255))
         light.setPixelColor(1, light.rgb(255, 255, 255))
         light.setPixelColor(2, light.rgb(255, 255, 255))
@@ -9,6 +9,13 @@ while (true) {
         light.setPixelColor(6, light.rgb(255, 255, 255))
         light.setPixelColor(7, light.rgb(255, 255, 255))
         light.setPixelColor(8, light.rgb(255, 255, 255))
+    }
+    
+    if (input.temperature(TemperatureUnit.Fahrenheit) < 68) {
+        music.powerDown.play()
+    }
+    
+    if (input.temperature(TemperatureUnit.Fahrenheit) < 72) {
         music.powerDown.play()
     }
     
